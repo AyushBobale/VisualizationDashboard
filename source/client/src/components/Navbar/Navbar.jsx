b423fe40ac3bf85bbe0f8723d2542bb0928d8906
Ayush Bobale <bobaleayush@gmail.com>
2023-04-18 22:58:36 +0530

    last todays

--- source/client/src/components/Navbar/Navbar.jsx
+++ source/client/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,11 @@ import "./Navbar.css";
 import React from "react";
 
 function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-inner">Navbar</div>
+    </div>
+  );
 }
 
 export default Navbar;
